fd7281aa8f53ab362c0abbb478658efc31ec92ef
Edward Tompkinson <ecptompkinson@hotmail.co.uk>
2026-04-03 12:28:34 +0100

    FizzBuzz added

---
+++ FizzBuzz/FizzBuzz.py
@@ -0,0 +1,18 @@
+
+# FizzBuzz
+# The aim of the FizzBuzz program is to output 15 lines to the terminal, "Fizz", "Buzz", "FizzBuzz", or a number.
+# A good method for doing this is to use a for loop, where a loop's counter increments on every iteration.
+# "Fizz" is outputted on every third loop.
+# "Buzz" is outputted on every fifth loop.
+# if neither "Fizz" or "Buzz" are output then the loop's counter is output as a numerical value.
+
+# Example for values 11 to 15:
+# ...
+# 11
+# Fizz
+# 13
+# 14
+# FizzBuzz
+
+limit = 16
+output = ""
