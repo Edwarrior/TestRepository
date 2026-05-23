9db40af576dcebbcf1464d2fce50a30613863257
Edward Tompkinson <ecptompkinson@hotmail.co.uk>
2026-05-23 08:52:22 +0100

    Game of Life Barebarebones
    Playing around with canvas

---
+++ GameOfLife/Display.py
@@ -0,0 +1,15 @@
+# Code incomplete, just testing for the moment
+from tkinter import *
+
+root = Tk()
+root.title("The Game of Life")
+
+canvas = Canvas(root, width=200, height=200)
+canvas.pack()
+
+canvas.create_rectangle(10,10,110,110)
+
+
+
+
+root.mainloop()
